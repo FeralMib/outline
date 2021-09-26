bd9590d58aee8db1cb19d8cd2699a8fe841fa494
Feral Mib <mibsolution@tutanota.com>
2021-09-25 23:24:47 -0700

    local changes

--- start.sh
+++ start.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 . $HOME/.profile
 cd $HOME/outline
-yarn start
+#yarn start
+
+# https://github.com/outline/outline/pull/1686/files
+NODE_ENV=production pm2 start ./build/server/index.js --name outline -i max
