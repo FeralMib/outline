7235d9053603122b5834113b839174844b556028
Feral Mib <mibsolution@tuta.com>
2021-12-15 07:51:54 +1100

    Local Development

--- start.sh
+++ start.sh
@@ -3,5 +3,16 @@
 cd $HOME/outline
 #yarn start
 
+## Update
+# yarn cache clean
+# cd rich-markdown-editor
+# yarn install
+# yarn build
+# npm pack
+# cd ..
+## double check pkg name matches package.json
+# yarn install
+# yarn build
+
 # https://github.com/outline/outline/pull/1686/files
 NODE_ENV=production pm2 start ./build/server/index.js --name outline -i max
